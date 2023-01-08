9d5cb902b5f050ebb6b00cc4e81bdfc02790cdb3
Kasjful Kurniawan <kasful.bpp@gmail.com>
2023-01-08 19:38:52 +0800

    fix(code): fixing name of variable

--- internal/handlers/book.go
+++ internal/handlers/book.go
@@ -7,7 +7,7 @@ import (
 	"github.com/kasfulk/golang-library/internal/databases/build"
 	dbFunctions "github.com/kasfulk/golang-library/internal/databases/functions"
 	"github.com/kasfulk/golang-library/internal/databases/schemas"
-	apperror "github.com/kasfulk/golang-library/internal/errors"
+	appError "github.com/kasfulk/golang-library/internal/errors"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
@@ -17,20 +17,20 @@ func BookIndex(c echo.Context) error {
 	redisResult, redisError := Redis.Get("books").Result()
 
 	if redisError != nil && redisResult != "" {
-		return c.JSON(apperror.RedisError())
+		return c.JSON(appError.RedisError())
 	}
 
 	if redisResult == "" || redisResult == "[]" {
 		books, booksError := dbFunctions.ReCacheBooks()
 		if booksError != nil {
-			return c.JSON(apperror.RedisError())
+			return c.JSON(appError.RedisError())
 		}
 		return c.JSON(http.StatusOK, books)
 	} else {
 		var resultData []schemas.Book
 		err := json.Unmarshal([]byte(redisResult), &resultData)
 		if err != nil {
-			return c.JSON(apperror.ServerError())
+			return c.JSON(appError.ServerError())
 		}
 		return c.JSON(http.StatusOK, resultData)
 	}
@@ -42,7 +42,7 @@ func BookDetail(c echo.Context) error {
 	post, err := dbFunctions.ShowBookDetail(id)
 	if err != nil {
 		if err == gorm.ErrRecordNotFound {
-			return c.JSON(apperror.NotFound())
+			return c.JSON(appError.NotFound())
 		}
 		return c.JSON(http.StatusInternalServerError, err)
 	}
@@ -53,7 +53,7 @@ func BookDelete(c echo.Context) error {
 	id := c.Param("id")
 	RowsAffected := dbFunctions.DeleteBook(id)
 	if RowsAffected == 0 {
-		return c.JSON(apperror.NotFound())
+		return c.JSON(appError.NotFound())
 	}
 	return c.JSON(http.StatusOK, map[string]string{
 		"message": "Data telah dihapus",
@@ -65,11 +65,11 @@ func BookCreate(c echo.Context) error {
 	err := c.Bind(book)
 
 	if err != nil {
-		return c.JSON(apperror.BadRequest())
+		return c.JSON(appError.BadRequest())
 	}
 
 	if err := dbFunctions.CreateBook(book); err != nil {
-		return c.JSON(apperror.BadRequest())
+		return c.JSON(appError.BadRequest())
 	}
 	return c.JSON(http.StatusOK, book)
 }
@@ -80,11 +80,11 @@ func BookUpdate(c echo.Context) error {
 	err := c.Bind(book)
 
 	if err != nil {
-		return c.JSON(apperror.BadRequest())
+		return c.JSON(appError.BadRequest())
 	}
 
 	if err := dbFunctions.UpdateBook(id, book); err != nil {
-		return c.JSON(apperror.ServerError())
+		return c.JSON(appError.ServerError())
 	}
 	return c.JSON(http.StatusOK, book)
 }
@@ -95,11 +95,11 @@ func BookedBy(c echo.Context) error {
 	err := c.Bind(book)
 
 	if err != nil {
-		return c.JSON(apperror.BadRequest())
+		return c.JSON(appError.BadRequest())
 	}
 
 	if err := dbFunctions.UpdateBook(id, book); err != nil {
-		return c.JSON(apperror.ServerError())
+		return c.JSON(appError.ServerError())
 	}
 	return c.JSON(http.StatusOK, book)
 }
